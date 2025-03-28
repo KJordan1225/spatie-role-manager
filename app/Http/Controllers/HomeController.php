adad05978e9217fa30d438172ad49721257a8e8e
KJordan1225 <shadow902@gmail.com>
2025-03-28 18:35:56 -0400

    add left sidebar nav bar to admin dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,17 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+        $rolesCollection = $user->getRoleNames();
+        $role = $rolesCollection->first();
+
+        if ($role === 'Admin')
+        {
+            return view('admindash');
+        } else {
+            return view('home');
+        }
+        // dd($role[0]);
+        
     }
 }
--- resources/views/admindash.blade.php
+++ resources/views/admindash.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.adminDashboard')
 
 @section('content')
 <div class="container">
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }} Non Admin User
+                    {{ __('You are logged in! Admin Dashboard') }}
                 </div>
             </div>
         </div>
